63bfd0522811db2f0def9e6963c3035e5b64af3e
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-03 16:01:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_fourth_tier
   hash[:railroads][:pieces] = 4
   hash[:railroads][:rent_in_dollars] = {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
   hash[:railroads][:names] = {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {} }
-  hash[:railroads][:names][:reading_railroad] = "$100"
+  hash[:railroads][:names][:reading_railroad][:mo] = "$100"
 end
 
 
